82ae6ea4c861d76b66bf084bc56f0391ed78c1ef
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-22 19:50:21 +0100

    adding short urls to the database

--- apps/rickandroll/src/main.ts
+++ apps/rickandroll/src/main.ts
@@ -12,29 +12,36 @@ const url = 'mongodb://mongoadmin:idealist42-@127.0.0.1:27018/'
 const client = new MongoClient(url)
 const dbName = 'rickandroll'
 
+let collection;
+
+const app = express();
+
 const connectToDatabase = async () => {
   await client.connect();
   console.log('Connected to database server')
-  const db =  client.db(dbName)
-  const collection = db.collection('urls')
+  const db = client.db(dbName)
+  collection = db.collection('urls')
+  // return collection;
 }
 
 connectToDatabase()
   .then(console.log)
   .catch(console.error)
-  .finally(() => client.close())
+  // .finally(() => client.close())
 
 
-const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to rickandroll!!!' });
 });
 
-app.get('/api/shorturl', (req: Request, res: Response) => {
+app.post('/api/shorturl', async (req: Request, res: Response) => {
   const randomString = randomstring.generate(6);
-  return res.json({ url: `http://localhost:3333/api/${randomString}`})
+  const insertResult = await collection.insert({ url: `http://localhost:3333/api/${randomString}`})
+  return res.json(insertResult)
 });
+// insert() returns a promise therefore async await
+
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
